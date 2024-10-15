f48311aee5c33e5b8dd51769f73ebd22d0530d79
ay936 <ay936@gmail.com>
2024-10-15 15:05:11 +0800

    first commit.

---
+++ src/javatest/IntOverflow.java
@@ -0,0 +1,20 @@
+package javatest;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class IntOverflow{
+	public static void main(String[] args){
+		
+		var height = new Scanner(System.in);
+		var weight = new Scanner(System.in);
+		double h = height .nextInt();
+		double w = weight .nextInt();
+		double bmi = w/Math.pow((h/100), 2);
+		System.out.println(Math.round(bmi*100.0)/100.0);
+		
+		height.close();
+		weight.close();
+
+	}
+}
---
+++ src/javatest/eclipseTest.java
@@ -0,0 +1,16 @@
+package javatest;
+
+public class eclipseTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=6;
+		byte c=100;
+		b=b+a;
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
